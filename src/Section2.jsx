ec64428aa58da44b74631a63f4b20979d78a4876
Filip <sieniu212@gmail.com>
2024-08-16 12:44:06 +0200

    Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	new file:   src/1.jpg 	new file:   src/2.jpg 	modified:   src/App.js 	modified:   src/Section1.jsx 	modified:   src/Section2.jsx 	new file:   src/Section3.jsx 	new file:   src/bg2.jpg 	modified:   src/index.css 	new file:   src/projects_gallery.css

--- src/Section2.jsx
+++ src/Section2.jsx
@@ -1,45 +1,107 @@
 const Section2 = () => {
-	return (
-		<div className="relative h-screen w-full bg-first flex justify-center pt-20">
-			<h1 className="absolute top-20 text-white text-5xl z-30 bg-first p-2">
-				Education
-			</h1>
-			<div className="flex w-10/12 justify-center">
-				<div className="flex flex-col items-end w-2/3 h-5/6 bg-secc z-20 mt-20">
-					<div className="flex w-2/3 m">
-						<p className="text-white text-2xl mt-16  px-2 font-thin">2017</p>
-						<div className=" h-48 border-t-4 border-accent1 border-dashed mt-20 flex flex-col">
-							<h1 className="text-white text-4xl font-light text-end pr-2">
-								Complex of Schools and Educational Institutions in Nysa.
-							</h1>
-							<h2 className="text-text text-2xl font-thin text-end pr-2">
-								Technician of Information Technology
-							</h2>
-						</div>
-					</div>
+	const education = [
+		{
+			name: "Complex of Schools and Educational Institutions in Nysa",
+			degree: "Technician of Information Technology",
+			year: "2017",
+		},
+		{
+			name: "University of Applied Sciences In Nysa",
+			degree: "Web Systems Engineer",
+			year: "2021",
+		},
+		{
+			name: "Degree in Computer Science",
+			degree: "Bachelor of Engineering",
+			year: "2025",
+		},
+	];
 
-					<div className="flex w-2/3 mt-36">
-						<p className="text-white text-2xl px-2 font-thin">2025</p>
-						<div className=" h-48 border-t-4 border-accent1 border-dashed flex flex-col mt-4">
-							<h1 className="text-white text-4xl font-light text-end pr-2">
-								Bachelor of Engineering in Computer Science
-							</h1>
-						</div>
-					</div>
+	const expirience = [
+		{
+			name: "Wakmet Sp. z. o. o.",
+			tasks: ["Technician of Information Technology"],
+			year: "2018",
+		},
+		{
+			name: "PP Malta Group",
+			degree: ["Web Systems Engineer"],
+			year: "2022",
+		},
+		{
+			name: "Głuchołazy City Hall",
+			degree: ["Bachelor of Engineering"],
+			year: "2024",
+		},
+	];
+
+	return (
+		<div className="relative h-screen w-screen bg-first flex justify-center overflow-hidden shadow-2xl shadow-zinc-900">
+			<div className="absolute w-[200px] h-[1px] border-t-2 border-accent1 z-30 rotate-90 top-32 left-32" />
+
+			<div className="flex w-10/12 justify-center">
+				<div className="flex flex-col items-end justify-around  w-2/3 h-5/6 bg-secc z-20 mt-20 py-10">
+					<h1 className="top-20 text-white text-5xl z-30 p-2 w-full flex justify-center mb-10">
+						Education
+					</h1>
+					{education.map((elem) => {
+						return (
+							<div className="flex w-3/4">
+								<p className="text-white text-2xl px-2 font-thin">
+									{elem.year}
+								</p>
+								<div className=" h-48 border-t-4 border-accent1 border-dashed flex flex-col mt-4">
+									<h1 className="text-white text-4xl font-light text-end pr-2">
+										{elem.name}
+									</h1>
+									<h2 className="text-text text-2xl font-thin text-end pr-2">
+										{elem.degree}
+									</h2>
+								</div>
+							</div>
+						);
+					})}
 				</div>
-				<div className="border-r-4 border-accent1 z-20 h-full flex-nowrap" />
-				<div className="w-2/3 h-5/6 bg-third z-20 mt-20 pt-4 flex">
-					<div className=" h-48 border-t-4 border-accent1 border-dashed mt-64 flex flex-col">
-						<h1 className="text-white text-4xl font-light pl-2">
-							University of Applied Sciences In Nysa
-						</h1>
-						<h2 className="text-text text-2xl font-thin pl-2">
-							Web Systems Engineer
-						</h2>
-					</div>
-					<p className="text-white text-2xl mt-60 px-2 font-thin">2021</p>
+				<div className="border-r-4 border-accent1 z-30 h-full absolute" />
+				<div className="flex flex-col items-start  justify-around w-2/3 h-5/6 bg-third z-20 mt-20 py-10 flex">
+					<h1 className="top-20 text-white text-5xl z-30 p-2 w-full flex justify-center mb-10">
+						Expirience
+					</h1>
+					{expirience.map((elem) => {
+						return (
+							<div className="flex w-3/4">
+								<div className=" h-48 border-t-4 border-accent1 border-dashed flex flex-col mt-4">
+									<h1 className="text-white text-4xl font-light text-end pr-2">
+										{elem.name}
+									</h1>
+									<h2 className="text-text text-2xl font-thin text-end pr-2">
+										{elem.degree}
+									</h2>
+								</div>
+								<p className="text-white text-2xl px-2 font-thin">
+									{elem.year}
+								</p>
+							</div>
+						);
+					})}
 				</div>
 			</div>
+
+			<div className="absolute w-[200px] h-[1px] border-t-2 border-accent1 z-20 rotate-90 bottom-32 right-32" />
+			{[...Array(100)].map((x, i) => {
+				let x_coord = Math.floor(Math.random() * 1920) - 100;
+				let y_coord = Math.floor(Math.random() * 800) + 50;
+				let x_or_y = Math.floor(Math.random() * 2);
+
+				return (
+					<div
+						className={`absolute w-[200px] h-[1px] border-t-2 border-text ${
+							x_or_y ? "rotate-90" : null
+						} z-10`}
+						style={{ left: `${x_coord}px`, top: `${y_coord}px` }}
+					/>
+				);
+			})}
 		</div>
 	);
 };
---
+++ src/Section3.jsx
@@ -0,0 +1,113 @@
+import React, { useRef, useEffect, useState } from "react";
+import bgimage from "./1.jpg";
+
+import "./projects_gallery.css";
+
+import "react-alice-carousel/lib/alice-carousel.css";
+
+import img1 from "./2.jpg";
+import img2 from "./2.jpg";
+import img3 from "./2.jpg";
+
+import { motion } from "framer-motion";
+
+const Section3 = () => {
+	const [width, setWidth] = useState(0);
+	const carousel = useRef();
+
+	useEffect(() => {
+		console.log(carousel.current.scrollWidth, carousel.current.offsetWidth);
+		setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth + 150);
+	}, []);
+
+	const projects = [
+		{
+			id: 1,
+			name: "First Portfolio",
+			image: img1,
+			link: "https://github.com/Sagittarius-py/Portfolio-One",
+			active: true,
+		},
+		{
+			id: 2,
+			name: "Second Portfolio",
+			image: img2,
+			link: "https://github.com/Sagittarius-py/Portfolio-Two",
+			active: false,
+		},
+		{
+			id: 3,
+			name: "Third Portfolio",
+			image: img3,
+			link: "https://github.com/Sagittarius-py/Portfolio-Three",
+			active: false,
+		},
+		{
+			id: 4,
+			name: "fourth",
+			link: "http://...",
+			active: false,
+		},
+		{
+			id: 5,
+			name: "fivth",
+			link: "http://...",
+			active: false,
+		},
+		{
+			id: 6,
+			name: "sixth",
+			link: "http://...",
+			active: false,
+		},
+	];
+
+	const openInNewTab = (url) => {
+		window.open(url, "_blank").focus();
+	};
+
+	return (
+		<div className="w-screen bg-first h-screen flex justify-center items-center relative bg-opacity-50">
+			<img
+				src={bgimage}
+				alt="bg"
+				className="w-screen h-screen filter contrast-150 saturate-75 absolute shadow-2xl shadow-zinc-900 object-cover -z-10"
+			/>
+			<div className="border-r-4 border-accent1 h-full flex-nowrap absolute " />
+			<div className="project-carousel">
+				<motion.div
+					ref={carousel}
+					className="carousel"
+					whileTap={{ cursor: "grabbing" }}
+				>
+					<motion.div
+						drag="x"
+						dragConstraints={{ right: 0, left: -width }}
+						className="inner-carousel"
+					>
+						{projects.map((project) => {
+							return (
+								<motion.div
+									style={{ backgroundImage: `url(${project.image})` }}
+									className="item"
+									key={project.id}
+								>
+									<div className="item-content">
+										<p
+											className="name"
+											onClick={() => openInNewTab(project.link)}
+										>
+											{project.name}
+										</p>
+									</div>
+								</motion.div>
+							);
+						})}
+					</motion.div>
+				</motion.div>
+			</div>
+		</div>
+	);
+};
+
+export default Section3;
